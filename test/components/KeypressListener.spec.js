05ae4d56cbeb24f68e1a90c233e6bbe06bd4be84
Mark Laane <marklaane@gmail.com>
2016-05-05 02:48:46 +0300

    Added tests for keypress

---
+++ test/components/KeypressListener.spec.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import KeypressListener from '../../es6/components/KeypressListener';
+
+describe('KeypressListener', () => {
+  var buildKeypressListener = ((handleKeypress) => {
+    return TestUtils.renderIntoDocument(
+      <KeypressListener handleKeyPress={handleKeypress} />
+    );
+  });
+
+  it('calls handleKeyPress with the key string when keypress triggered on window', () => {
+    let handleKeypress = sinon.stub()
+    buildKeypressListener(handleKeypress);
+
+    var event = new window.KeyboardEvent('keypress', {keyCode: 69})
+    window.dispatchEvent(event)
+    expect(handleKeypress).to.have.been.calledWith("E")
+  });
+
+});
