1c697e9bb81bea8f8b1c9a08b1ad7c8715002094
Mark Laane <marklaane@gmail.com>
2016-05-10 12:31:30 +0300

    not loading proxy, when in production.

--- server.js
+++ server.js
@@ -5,45 +5,37 @@ var isProduction = process.env.NODE_ENV === 'production';
 var port = isProduction ? process.env.PORT : 3000;
 var publicPath = path.resolve(__dirname, 'public');
 
-var WebSocketServer = require('websocket').server
+//Express
 var express = require('express');
-var httpProxy = require('http-proxy');
-var proxy = httpProxy.createProxyServer();
 var app = express();
 var server = app.listen(port, () => console.log('Server running on port ' + port));
+
+//Websockets
+var WebSocketServer = require('websocket').server
 var wsServer = new WebSocketServer({ httpServer : server });
 wsServer.on('request', handleIncomingRequest);
 
 
-// We only want to run the workflow when not in production
 if (!isProduction) {
-
-  // We require the bundler inside the if block because
-  // it is only needed in a development environment. Later
-  // you will see why this is a good idea
   var bundleSource = require('./server/bundleSource.js');
   bundleSource();
 
   // Any requests to localhost:3000/build is proxied
   // to webpack-dev-server
-  app.all('/build/*', function (req, res) {
-    proxy.web(req, res, {
-        target: 'http://localhost:8080'
-    });
-  });
-
+  var httpProxy = require('http-proxy');
+  var proxy = httpProxy.createProxyServer();
+  // It is important to catch any errors from the proxy or the server will crash. 
+  proxy.on('error', () => console.log('Could not connect to proxy, please try again...'));
+  app.all(
+    '/build/*', 
+    (req, res) => proxy.web(req, res, {target: 'http://localhost:8080'})
+  );
 }
 
+//Serve static files
 app.use(express.static(publicPath));
-
+//If no static file found, serve index.html
 app.get('/*', function(req, res){
   res.sendFile(publicPath + '/index.html');
-});
-
-// It is important to catch any errors from the proxy or the
-// server will crash. An example of this is connecting to the
-// server when webpack is bundling
-proxy.on('error', function(e) {
-  console.log('Could not connect to proxy, please try again...');
 });
 
